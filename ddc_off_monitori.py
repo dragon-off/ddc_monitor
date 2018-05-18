5e4252e033c1273667d572fac88cdb87f2c19f0d
Anton Degterenko <dragon-off@dragon-off.ru>
2018-05-18 14:17:43 +0300

    пример выключения включения монитора через winAPI из Python. возможно регулировать яркость...

---
+++ ddc_off_monitori.py
@@ -0,0 +1,42 @@
+from ctypes import windll, byref, Structure, WinError, POINTER, WINFUNCTYPE
+from ctypes.wintypes import BOOL, HMONITOR, HDC, RECT, LPARAM, DWORD, BYTE, WCHAR, HANDLE
+
+_MONITORENUMPROC = WINFUNCTYPE(BOOL, HMONITOR, HDC, POINTER(RECT), LPARAM)
+
+class _PHYSICAL_MONITOR(Structure):
+    _fields_ = [('handle', HANDLE),
+                ('description', WCHAR * 128)]
+
+def _iter_physical_monitors(close_handles=True):
+    def callback(hmonitor, hdc, lprect, lparam):
+        monitors.append(HMONITOR(hmonitor))
+        return True
+
+    monitors = []
+    if not windll.user32.EnumDisplayMonitors(None, None, _MONITORENUMPROC(callback), None):
+        raise WinError('EnumDisplayMonitors failed')
+
+    for monitor in monitors:
+        # get physical monitor count
+        count = DWORD()
+        if not windll.dxva2.GetNumberOfPhysicalMonitorsFromHMONITOR(monitor, byref(count)):
+            raise WinError()
+        # get physical monitor handles
+        physical_array = (_PHYSICAL_MONITOR * count.value)()
+        if not windll.dxva2.GetPhysicalMonitorsFromHMONITOR(monitor, count.value, physical_array):
+            raise WinError()
+        for physical in physical_array:
+            yield physical.handle
+            if close_handles:
+                if not windll.dxva2.DestroyPhysicalMonitor(physical.handle):
+                    raise WinError()
+
+
+def set_vcp_feature(monitor, code, value):
+    if not windll.dxva2.SetVCPFeature(HANDLE(monitor), BYTE(code), DWORD(value)):
+        raise WinError()
+
+for handle in _iter_physical_monitors():
+	# on- 0x01; off- 0x04
+    set_vcp_feature(handle, 0xd6, 0x04) 
+   
