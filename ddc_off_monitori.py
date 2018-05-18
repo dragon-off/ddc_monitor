7cb6a873617b589c652e6d7f20c45e94ea35aab2
Anton Degterenko <dragon-off@dragon-off.ru>
2018-05-18 15:56:41 +0300

    Update ddc_off_monitori.py

--- ddc_off_monitori.py
+++ ddc_off_monitori.py
@@ -38,5 +38,14 @@ def set_vcp_feature(monitor, code, value):
 
 for handle in _iter_physical_monitors():
 	# on- 0x01; off- 0x04
+	#Restore Factory Defaults 0x04 set >0
+	#Restore Factory Luminance/ Contrast Defaults 0x05 set >0
+	#Restore Factory Color Defaults 0x08 set >0
+	#Luminance 0x10 set 0-100
+	#Contrast 0x12 set 0-100
+	#Power Mode 0xD6 set 0x01-on,0x05-off
+	#Input Source 0x60 set 0x01-D-sub; 0x03-DVI; 0x11-HDMI
+	#Audio: Speaker Volume 0x62 set >0
+	#Audio Mute 0x8D set 0x01-on, 0x02-off
     set_vcp_feature(handle, 0xd6, 0x04) 
    
